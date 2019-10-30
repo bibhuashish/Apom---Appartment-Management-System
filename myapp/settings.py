9533a910de8ab12d79bf65ddaa708107830958a6
paruchurimadhurima <madhurimaparuchuri@gmail.com>
2019-10-31 00:04:07 +0530

    fifth commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'apomdb',
         'HOST': 'localhost',
